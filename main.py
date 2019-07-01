c6d57b5ef0d000f9a489db21428303e5b4f94af5
Yuki Nagae <yuki.nagae1130@gmail.com>
2019-07-01 21:58:24 +0900

    Make it as a flask app

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from dotenv import load_dotenv
+from os.path import join, dirname
+import os
 import tweepy
 
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
+from flask import Flask, jsonify
+app = Flask(__name__)
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -14,20 +16,33 @@ access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 
 user_name = 'yukinagae'  # TODO: dummy user
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-timelines = api.user_timeline(user_name)
 
-for status in timelines[:20]:
-    if status.retweeted:
-        print(f"{status.text[:20]}:{status.retweeted}")
-        # TODO: this way of getting urls is not perfect
-        # It misses some of the urls for some reasons.
-        # maybe:
-        # - `retweeted_status.entities['urls']`
-        # as well.
-        for url in status.entities['urls']:
-            print(url['expanded_url'])
+@app.route("/tweets")
+def tweets():
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    timelines = api.user_timeline(user_name)
+
+    urls = []
+
+    for status in timelines[:100]:
+        if status.retweeted:
+            # TODO: this way of getting urls is not perfect
+            # It misses some of the urls for some reasons.
+            # maybe:
+            # - `retweeted_status.entities['urls']`
+            # as well.
+            for url in status.entities['urls']:
+                # print(f"{status.text[:20]}:{status.retweeted}")
+                urls.append(url['expanded_url'])
+                # print(url['expanded_url'])
+    return jsonify(urls)
